723f69cda81e1f1984e92c1f3d3946e192553a5f
Sam Vevang <sam.vevang@gmail.com>
2018-01-25 09:46:36 -0600

    run rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(tokenize("hello there"), ["hello", "there"]);
     }
 }
 
@@ -10,20 +11,17 @@ extern crate natural;
 use natural::tokenize::tokenize;
 
 extern crate libc;
-use std::ffi::{CString, CStr };
+use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 
 fn into_safe_string(message: *const c_char) -> String {
-    unsafe {
-        CStr::from_ptr(message).to_string_lossy().into_owned()
-    }
+    unsafe { CStr::from_ptr(message).to_string_lossy().into_owned() }
 }
 
-
 #[no_mangle]
-pub extern fn hello_rust(raw: *const c_char) -> *const c_char {
-
+pub extern "C" fn hello_rust(raw: *const c_char) -> *const c_char {
     let incoming_message = into_safe_string(raw);
+
     let mut tokens = tokenize(&incoming_message);
     tokens.dedup();
 
